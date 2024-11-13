bdb7c1f46c6107d7f675ab697a71f414b40e835e
Nicole Barnhouse <104863678+nbarnhouse@users.noreply.github.com>
2024-11-12 21:00:11 -0600

    modified the index screen and login screens

--- barber-buddy-mobile/app/_layout.tsx
+++ barber-buddy-mobile/app/_layout.tsx
@@ -1,10 +1,20 @@
-// import { Stack } from 'expo-router';
+import { Stack } from 'expo-router';
 
-// export default function RootLayout() {
-//   return (
-//     <Stack>
-//       <Stack.Screen name="index" />
-//       <Stack.Screen name="login" />
-//     </Stack>
-//   );
-// }
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{ title: 'Home', headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="login"
+        options={{
+          title: 'Login',
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+}
