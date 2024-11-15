09d440aada7e44f8cd4a97d84e38418cffd16d0b
Nicole Barnhouse <104863678+nbarnhouse@users.noreply.github.com>
2024-11-15 17:26:18 -0600

    console log for testing added to buttons

--- barber-buddy-mobile/app/_layout.tsx
+++ barber-buddy-mobile/app/_layout.tsx
@@ -4,15 +4,15 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen
+        name="index"
+        options={{ title: 'Home', headerShown: false }}
+      />
+      <Stack.Screen
         name="login"
         options={{
           title: 'Login',
           headerShown: false,
         }}
-      />
-      <Stack.Screen
-        name="index"
-        options={{ title: 'Home', headerShown: false }}
       />
     </Stack>
   );
