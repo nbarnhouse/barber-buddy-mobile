a25b3e82d6a87e3102c2d553f12e74c5e5b8829d
Nicole Barnhouse <104863678+nbarnhouse@users.noreply.github.com>
2024-11-25 18:08:51 -0700

    added additional image and started on home_screen

--- barber-buddy-mobile/app/home.js
+++ barber-buddy-mobile/app/home.js
@@ -1,8 +1,31 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 export default function Home() {
+  const userLogo = require('../assets/images/logo_image.jpeg');
+  const homeButton = require('../assets/images/home_icon.png');
+
   return (
-    <View>
-      <Text>Home View</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <View>
+          <Image source={userLogo} style={styles.user_logo} />
+          <Text>Getting Started</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  user_logo: {
+    height: 82,
+    width: 82,
+  },
+});
