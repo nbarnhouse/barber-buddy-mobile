a61de75511df6f8cf1d5690c9a34d93ba4df8def
Nicole Barnhouse <nicolebarnhouse@gmail.com>
2025-01-10 21:22:36 -0600

    added sign in screen and asset dependencies

---
+++ app/globalStyles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  title: {
+    fontSize: 38.63,
+    color: '#FED992',
+    fontFamily: 'interFont',
+    lineHeight: 46.75,
+  },
+
+  text: {
+    fontSize: 20,
+    fontFamily: 'interFont',
+    color: '#FED992',
+  },
+});
+
+export default globalStyles;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,56 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { useRouter } from 'expo-router';
+
+import globalStyles from './globalStyles.js';
+import NonGradientButton from '../components/buttons/NonGradientButton.js';
 
 export default function Index() {
+const router = useRouter();
+const logoImg = require('../assets/images/barber_logo.png');
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo}></Image>
+      <Text style={globalStyles.title}>Barber Buddy</Text>
+      <Text style={[globalStyles.text, styles.text]}>
+        Getting a haircut can feel like entering uncharted territory. Questions
+        flood your mind: What style suits me? How do I explain it to my barber?
+        Will I end up with a haircut I hate? These uncertainties often deter men
+        from experimenting with new looks and styles.
+      </Text>
+      <Text style={[globalStyles.text, styles.text]}>
+        Imagine having a trusted companion by your side, providing expert
+        guidance and advice on the perfect haircut for you. That's exactly what
+        BarberBuddy offers. Our user-friendly mobile application is designed to
+        empower you with the knowledge and confidence you need to step into any
+        barbershop with ease.
+      </Text>
+
+      <NonGradientButton
+        label="Start"
+        height={42}
+        width={124}
+         onPress={() => router.push('/sign-in')}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0c2025',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    height: 140,
+    width: 140,
+  },
+  text: {
+    justifyContent: 'flex-start',
+    padding: 20,
+    fontSize: 18,
+  },
+});
+
